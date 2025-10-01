9bf0edc88d37efe0bf5a7c11d3f594f8baf86284
jesmarver <jesusperike@gmail.com>
2025-10-01 13:48:58 +0200

    feat(file-upload): Controller and router added for file-upload

---
+++ src/presentation/file-upload/routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { AuthMiddleware } from "../middlewares/auth.middleware";
+import { CategoryService } from "../services/category.service";
+import { FileUploadController } from "./controller";
+
+export class FileUploadRoutes {
+
+    static get routes(): Router {
+        const router = Router();
+
+        const controller = new FileUploadController();
+
+        // api/upload/single/<user|category|product>/
+        // api/upload/multiple/<user|category|product>/
+        router.post('/single/:type', controller.uploadFile);
+        router.post('/multiple/:type', controller.uploadMultipleFiles);
+
+        return router;
+    }
+}
