9ffc05ce369bec6e2c3f96c33dc5fcb6ca37b881
jesmarver <jesusperike@gmail.com>
2025-10-02 10:31:02 +0200

    feat(images): Endpoints added for retrieve images

--- src/presentation/file-upload/controller.ts
+++ src/presentation/file-upload/controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
 import { CustomError } from "../../domain";
+import { FileUploadService } from "../services/file-upload.service";
+import { UploadedFile } from "express-fileupload";
 
 
 export class FileUploadController {
 
     constructor(
-        // private readonly categoryService: CategoryService
+        private readonly fileUploadService: FileUploadService
     ) { }
 
     private handleError(error: unknown, res: Response) {
@@ -18,12 +20,24 @@ export class FileUploadController {
     }
 
     uploadFile = async (req: Request, res: Response) => {
-        res.json("uploadFile");
+        const type = req.params.type;
+
+        const file = req.body.files.at(0) as UploadedFile;
+
+        this.fileUploadService.uploadSingle(file, `uploads/${type}`)
+        .then(uploaded => res.json(uploaded))
+        .catch(error => this.handleError(error, res));
 
     };
 
     uploadMultipleFiles = async (req: Request, res: Response) => {
-        res.json("uploadMultipleFiles");
+        const type = req.params.type;
+        
+        const files = req.body.files as UploadedFile[];
+
+        this.fileUploadService.uploadMultiple(files, `uploads/${type}`)
+        .then(uploaded => res.json(uploaded))
+        .catch(error => this.handleError(error, res));
 
     };
 
--- src/presentation/file-upload/routes.ts
+++ src/presentation/file-upload/routes.ts
@@ -1,14 +1,19 @@
 import { Router } from "express";
-import { AuthMiddleware } from "../middlewares/auth.middleware";
-import { CategoryService } from "../services/category.service";
 import { FileUploadController } from "./controller";
+import { FileUploadService } from "../services/file-upload.service";
+import { FileUploadMiddleware } from "../middlewares/file-upload.middleware";
+import { TypeMiddleware } from "../middlewares/type.middleware";
 
 export class FileUploadRoutes {
 
     static get routes(): Router {
         const router = Router();
 
-        const controller = new FileUploadController();
+        const fileUploadService = new FileUploadService();
+        const controller = new FileUploadController(fileUploadService);
+
+        router.use(FileUploadMiddleware.containFiles);
+        router.use(TypeMiddleware.validTypes(['users', 'products', 'categories']));
 
         // api/upload/single/<user|category|product>/
         // api/upload/multiple/<user|category|product>/
